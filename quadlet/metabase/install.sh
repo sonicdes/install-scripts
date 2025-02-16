2315d7babfe2de013b17f9df0010c62e80569891
Denis Sadomowski <sonicdes@gmail.com>
2025-02-16 14:26:01 +0100

    update metabase install script

--- quadlet/metabase/install.sh
+++ quadlet/metabase/install.sh
@@ -1,3 +1,3 @@
 mkdir -p $HOME/.config/containers/systemd/metabase
-cp metabase-app.container  metabase-db.container  metabase-db.volume  metabase.network $HOME/.config/containers/systemd/metabase/
+cp metabase-app.container metabase-db.container metabase-db.volume metabase-upload-db.container metabase-upload-db.volume metabase.network $HOME/.config/containers/systemd/metabase/
 systemctl --user daemon-reload
