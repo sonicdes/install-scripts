a9496892db240eb07738e73c8342b92dd5ee6e97
Denis Sadomowski <sonicdes@gmail.com>
2026-03-07 23:30:12 +0100

    fix n8n install script

--- quadlet/n8n/install.sh
+++ quadlet/n8n/install.sh
@@ -1,3 +1,3 @@
-mkdir -p $HOME/.config/containers/systemd/metabase
+mkdir -p $HOME/.config/containers/systemd/n8n
 cp n8n-app.container n8n-db.container n8n-db.volume n8n.network $HOME/.config/containers/systemd/n8n/
 systemctl --user daemon-reload
